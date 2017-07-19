fc602ba5b62ac78173a4fe6b2f828f798c514d7e
linhanGE <linhan.ge@gmail.com>
2017-07-19 21:50:05 +1000

    single bubble particle with XDLVO

---
+++ Single_particle_bubble/parCFDDEMrun.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+#===================================================================#
+# allrun script for testcase as part of test routine 
+# run settlingTest CFD part
+# Christoph Goniva - Feb. 2011
+#===================================================================#
+
+#- source CFDEM env vars
+. ~/.bashrc
+
+#- include functions
+source $CFDEM_SRC_DIR/lagrangian/cfdemParticle/etc/functions.sh
+
+#--------------------------------------------------------------------------------#
+#- define variables
+casePath="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
+logpath=$casePath
+headerText="Test_LSFB"
+logfileName="log_$headerText"
+solverName="cfdemSolverPiso"
+nrProcs="2"
+machineFileName="none"   # yourMachinefileName | none
+debugMode="off"          # on | off| strict | profile
+testHarnessPath="$CFDEM_TEST_HARNESS_PATH"
+runOctave="false"
+postproc="false"
+cleanUp="false"
+#--------------------------------------------------------------------------------#
+
+#- call function to run a parallel CFD-DEM case
+parCFDDEMrun $logpath $logfileName $casePath $headerText $solverName $nrProcs $machineFileName $debugMode
+
+
+if [ $runOctave == "true" ]
+    then
+        #------------------------------#
+        #  octave
+
+        #- change path
+        cd octave
+
+        #- rmove old graph
+        rm cfdemSolverPiso_ErgunTestMPI.png
+
+        #- run octave
+        octave totalPressureDrop.m
+
+        #- show plot 
+        eog cfdemSolverPiso_ErgunTestMPI.png
+
+        #- copy log file to test harness
+        cp ../../$logfileName $testHarnessPath
+        cp cfdemSolverPiso_ErgunTestMPI.png $testHarnessPath
+fi
+
+if [ $postproc == "true" ]
+  then
+
+    #- keep terminal open (if started in new terminal)
+    echo "simulation finished? ...press enter to proceed"
+    read
+
+    #- get VTK data from liggghts dump file
+    cd $casePath/DEM/post
+    python -i $CFDEM_LPP_DIR/lpp.py dump*.liggghts_run
+
+    #- get VTK data from CFD sim
+    cd $casePath/CFD
+    foamToVTK                                                   #- serial run of foamToVTK
+    #source $CFDEM_SRC_DIR/lagrangian/cfdemParticle/etc/functions.sh                       #- include functions
+    #pseudoParallelRun "foamToVTK" $nrPostProcProcessors          #- pseudo parallel run of foamToVTK
+
+    #- start paraview
+    paraview
+
+    #- keep terminal open (if started in new terminal)
+    echo "...press enter to clean up case"
+    echo "press Ctr+C to keep data"
+    read
+
+fi
+
+#- clean up case
+if [ $cleanUp == "true" ]
+  then 
+    echo "deleting data at: $casePath ?\n"
+    read
+    source $WM_PROJECT_DIR/bin/tools/CleanFunctions
+    cd $casePath/CFD
+    cleanCase
+    rm -r $casePath/CFD/clockData
+    rm $casePath/CFD/octave/octave-workspace
+    rm -r $casePath/CFD/hpctoolkit-*
+    rm $casePath/DEM/post/*.*
+    touch $casePath/DEM/post/.gitignore
+    #rm $casePath/DEM/post/restart/*.*
+    rm $casePath/DEM/post/restart/liggghts.restartCFDEM*
+    touch $casePath/DEM/post/restart/.gitignore
+    echo "done"
+fi
+
+
